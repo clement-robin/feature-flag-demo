15ac69a03de4ed5f378118bdf91f147167453af0
clement-robin <robin.clement78@gmail.com>
2025-05-27 18:23:38 +0200

    path <Image> cart

--- app/components/Cart/CartItemDescription.tsx
+++ app/components/Cart/CartItemDescription.tsx
@@ -9,10 +9,18 @@ export default function CartItemDescription({
   color: string;
   image: string;
 }) {
+  const imageUrl = image.startsWith("http")
+    ? image
+    : `https://clement-robin.github.io/feature-flag-demo${image}`;
+
   return (
     <div>
       <div className="flex justify-between items-center gap-4">
-        <img src={image} alt={name} className="w-16 h-16 " />
+        <img
+          src={imageUrl}
+          alt={name}
+          className="w-16 h-16 object-cover rounded"
+        />
         <div className="w-full">
           <h2 className="font-semibold">{name}</h2>
           <p className="text-sm text-gray-600">
